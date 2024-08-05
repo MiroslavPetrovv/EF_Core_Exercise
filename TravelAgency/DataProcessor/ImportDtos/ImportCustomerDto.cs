52d25de85f7ea039e89f36351e2b82d0e2d8ae37
Miroslav Petrov <114015697+MiroslavPetrovv@users.noreply.github.com>
2024-08-05 14:00:55 +0300

    Adding Dtos

---
+++ TravelAgency/DataProcessor/ImportDtos/ImportCustomerDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.DataProcessor.ImportDtos
+{
+    using static TravelAgency.Data.DataConstraits;
+
+    [XmlType(nameof(Customer))]
+
+    public class ImportCustomerDto
+    {
+        [XmlElement("FullName")]
+        [Required]
+        [MaxLength(CustomerFullNameMaxLength)]
+        [MinLength(CustomerFullNameMinLength)]
+        public string FullName { get; set; } = null!;
+
+        [XmlElement("Email")]
+        [Required]
+        [MaxLength(CustomerEmailMaxLength)]
+        [MinLength(CustomerEmailMinLength)]
+        public string Email { get; set; } = null!;
+
+        [XmlAttribute("phoneNumber")]
+        [Required]
+        [RegularExpression(CustomerPhoneNumberPattern)]
+        public string PhoneNumber { get; set; } = null!;
+
+
+    }
+}
