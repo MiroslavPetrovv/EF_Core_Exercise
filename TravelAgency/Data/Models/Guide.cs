4923c3033f82aa21a45ed6fdfc0e01cde8dbc57d
Miroslav Petrov <114015697+MiroslavPetrovv@users.noreply.github.com>
2024-08-05 07:55:43 +0300

    Update Guide.cs

--- TravelAgency/Data/Models/Guide.cs
+++ TravelAgency/Data/Models/Guide.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelAgency.Data.Models.Enums;
 
 namespace TravelAgency.Data.Models
 {
+    using static DataConstraits;
     public class Guide
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GuideFullNameMaxLength)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public Language Language{ get; set; }
+
+        public virtual ICollection<TourPackageGuide> TourPackagesGuides { get; set; } = new List<TourPackageGuide>();
     }
 }
